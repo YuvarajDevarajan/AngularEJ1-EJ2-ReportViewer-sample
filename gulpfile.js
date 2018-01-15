53e71e7c5cdf9d3416fe36ff2ab888477e06ac55
Yuvaraj Devarajan <yuvaraj.devarajan@syncfusion.com>
2018-01-15 13:15:02 +0530

    Changes merged

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,7 @@
 
 var gulp = require('gulp');
 
-/**
- * Compile styles
- */
-gulp.task('styles', function() {
-    var sass = require('gulp-sass');
-    return gulp.src(['./styles/**/*.scss'], { base: './' })
-        .pipe(sass({
-            outputStyle: 'expanded',
-            includePaths: './node_modules/@syncfusion/'
-        }))
-        .pipe(gulp.dest('.'));
-});
+
 
 gulp.task('bundle', function(done) {
     var webpack = require('webpack');
